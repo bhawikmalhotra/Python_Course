b91fad554b6161ba49a63deeadb22559feae3209
bhawik <bhawikmalhotra000@gmail.com>
2025-12-08 11:46:14 +0530

    08-12-2025-11-46 14.11:

--- query.sql
+++ query.sql
@@ -1,17 +1,10 @@
-CREATE TABLE students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    grade VARCHAR(10)
-);
-
-INSERT INTO students (name, age, grade) VALUES
-('Aarav', 16, '10th'),
-('Riya', 15, '9th'),
-('Kabir', 17, '11th'),
-('Neha', 14, '8th'),
-('Arjun', 18, '12th');
-
-SELECT * FROM students;
-
 SHOW databases;
+
+use test;
+
+select * from customers;
+
+SELECT *
+FROM customers
+WHERE `First Name` = 'Linda';
+
